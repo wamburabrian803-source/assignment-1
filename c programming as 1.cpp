01e1e751242ba0e3da12e94ad438a71dc348c46a
wamburabrian803-source <wamburabrian803@gmail.com>
2025-09-18 14:27:20 +0300

    Add files via upload

---
+++ c programming as 1.cpp
@@ -0,0 +1,30 @@
+//
+/*
+Name:BRIAN GACHOKI WAMBURA
+Reg:CT100/G/26160/25
+Description:user details
+*/
+
+#include<stdio.h>
+//main function
+int main(){
+	
+	int phone_no;
+	printf("Enter your phone_no");
+	scanf("%d,&phone_no");
+	printf("your phone_no is %d",phone_no);
+    int weight;
+	printf("Enter your weight");
+	scanf("%d,&weight");
+	printf("your weight is %d",weight);
+	int height;
+	printf("Enter your height");
+	scanf("%d",&height);
+	printf("your height is %d\n",height);
+	return 0;
+  	
+}
+
+	
+	
+ 
---
+++ simle c programming 1.cpp
@@ -0,0 +1,23 @@
+//simple c program 
+/*
+Name:Brian Gachoki Wambura
+REG:CT100/G/26160/25
+DESCRIPTION:semester units
+*/
+
+#include<stdio.h>
+int main(){
+	printf("Analogue electronics/n");
+	printf("fundamentals of ICT/n");
+	printf("intoduction to computers appliaction/n");
+	printf("essentials of accounting /n");
+	printf("mathematics for science/n");
+	printf("computer system and organizations/n");
+	printf("introduction to programming and algorithms/n");
+	return 0;
+    }
+
+
+
+	
+	
