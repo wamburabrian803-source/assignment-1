8b6eb67a520c4dfc442d531e8946d2ff410e5eb2
wamburabrian803-source <wamburabrian803@gmail.com>
2025-09-25 01:01:44 +0300

    Add files via upload

---
+++ c compling surface area.c
@@ -0,0 +1,30 @@
+/*
+Name:Brian Gachoki
+Reg no:CT100/G/26160/25
+Description:Program to compute the surface_area
+*/
+
+#include <stdio.h>
+int main () {
+	//variable declaration
+
+	float pie, radius ,height , surface_area;
+	pie=3.142;
+     
+    //prompt the user to enter radius
+   printf("\n Enter radius: ");
+    scanf("%f", &radius);
+    
+    //prompt the user to enter height
+    printf("\n Enter height: ");
+    scanf("%f", &height); 
+    
+    //compute surface_area
+    
+    surface_area = (2 * pie * radius * radius) + (2 * pie * radius * height);
+    
+    //display surface area
+    printf("\n surface_area is %.2f", surface_area);
+    
+    return 0;
+}
